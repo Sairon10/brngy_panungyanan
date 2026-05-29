341c9d8e975aec80a6d36dd2c4c1b681923d9588
Sairon10 <saironmarkmanalad71@gmail.com>
2026-05-29 21:22:20 +0800

    Navbar Update

--- partials/footer.php
+++ partials/footer.php
@@ -5,7 +5,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-<script src="public/js/navbar-enhancements.js?v=<?php echo time(); ?>"></script>
+<script src="public/js/navbar-script.js?v=<?php echo time(); ?>"></script>
 <script src="public/js/chatbot.js?v=<?php echo time(); ?>"></script>
 </body>
 
